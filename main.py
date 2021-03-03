cf05121c479ed1ac3017005341ed5b910bf8cc15
MitchellMarczak <memarczak2025@gmail.com>
2021-03-03 16:51:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print()
+print("Project One: Identity Jam")
+print()
+print()
+
+first_name = "Mitchell"
+last_name = "Marczak"
+age = 13
+print(first_name, last_name, age)
+print()
+age = 13 + 1
+print(age, last_name)
+print()
+age = 13
+school_grade = age - 5
+print(school_grade,"th grade")
+print()
+x = 8 - school_grade
+print("I will enter highschool in", x, "years!")
